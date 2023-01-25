5056e828b03d97de41fb16968300a0c884a972a2
jc <juan.cruz.business@gmail.com>
2023-01-25 11:45:54 -0600

    Setup BACK behaviour

--- gemt_proto1.ino
+++ gemt_proto1.ino
@@ -26,10 +26,15 @@ int main (void)
     "Ultrasonic Sensor Test"
     
   };
+
+  // Submenu for servo
+  // Note: Submenus should have back to return to previos menu
+  // TODO: Find better way of incorporating menu heirchy
   const char* servoMenu[] = 
   {
     "Manual Operation Test",
-    "Automatic Operation Test"
+    "Automatic Operation Test",
+    "Back"
   };
   
   // infinite loop
@@ -37,36 +42,49 @@ int main (void)
   {
     // Main Menu switch    
       // Get user Serial input for desired main menu test
-    unsigned short int sel = menuSelection("Main Menu", mainMenu, (sizeof(mainMenu) / sizeof(char *))); 
+    unsigned short int mainSel = menuSelection("Main Menu", mainMenu, (sizeof(mainMenu) / sizeof(char *))); 
                               // Note: Division specifies the number of elements (ie, the number of char pointers) in the array of pointers
-    switch (sel)
+    switch (mainSel)
     {
       case 1: // 9G
       {
         String servoConnectionInfoMsg = "Connections:\n+ -> 5V\n- -> GND\nPWM pin -> 9\n";
+        bool localExit = false; // Exit flag when user selects 'Back'
 
-        // Servo submenu switch
-        unsigned short int subSel = menuSelection("9G Servo Menu", servoMenu, (sizeof(servoMenu) / sizeof(char *)));  
-       
-        switch(subSel)
+        // while loop to allow user to go back to previous screen
+        while (localExit != true)
         {
-          case 1: // Manual mode
+          // Servo submenu switch
+          unsigned short int subSel = menuSelection("9G Servo Menu", servoMenu, (sizeof(servoMenu) / sizeof(char *)));  
+          switch(subSel)
           {
-            bool proceed;
-            proceed = infoScreen(servoConnectionInfoMsg);
+            case 1: // Manual mode
+            {
+              bool proceed;
+              proceed = infoScreen(servoConnectionInfoMsg);
 
+              if (proceed == true)
+              {
+                //run test
+              }
 
-            break;
-          }
-          case 2: // Auto mode
-          {
+              break;
+            }
+            case 2: // Auto mode
+            {
 
-            break;
-          }
-          default:
-          {
-            Serial.println("Error: Invalid inputs should be caught by getSerrialInput_int() !");
-          }
+              break;
+            }
+            case 3: // Back
+            {
+              localExit = true;
+              break;
+            }
+            default:
+            {
+              Serial.println("Error: Invalid inputs should be caught by getSerrialInput_int() !");
+            }
+          }        
         }
         
         break;
@@ -75,7 +93,7 @@ int main (void)
       {
         bool proceed;
         
-        Serial.println(sel); // DEBUG
+        Serial.println(mainSel); // DEBUG
 
         proceed = infoScreen("This is a test msg with no real information. Sorry!");
         Serial.println(proceed); // DEBUG
@@ -84,19 +102,19 @@ int main (void)
       }
       case 3: // nRF
       {
-        Serial.println(sel); // DEBUG
+        Serial.println(mainSel); // DEBUG
 
         break;
       }
       case 4: // L298N
       {
-        Serial.println(sel); // DEBUG
+        Serial.println(mainSel); // DEBUG
 
         break;
       }
       case 5: // Ultrasonic
       {
-        Serial.println(sel); // DEBUG
+        Serial.println(mainSel); // DEBUG
 
         break;
       }
